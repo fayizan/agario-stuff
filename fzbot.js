d399b9d94f1ce8ed4d5edb71d152279049fe8c5b
Fz <fayizan@gmail.com>
2016-10-02 22:38:29 +0530

    Update fzbot.js

--- fzbot.js
+++ fzbot.js
@@ -20,7 +20,7 @@ function updateBotCount(a, b) {
   if (0 == c) {
     $("#botCount").html('<font color="red">0 / ' + d + "</font>");
   } else {
-    $("#botCount").html('<font color="#7FFF00">' + c + " / " + 20 + "</font>");
+    $("#botCount").html('<font color="#7FFF00">' + c + " / " + 50 + "</font>");
   }
 }
 function startLocalBots() {
